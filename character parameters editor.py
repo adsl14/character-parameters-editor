f68a5c9006798c9a6c86cfb992dc04330319065d
i32camad <ferrari_dani_010@hotmail.es>
2021-08-24 03:12:50 +0200

    Uploaded the images

--- character parameters editor.py
+++ character parameters editor.py
@@ -1,12 +1,44 @@
-import os
+import os, stat, functools
 
+from shutil import rmtree
+from datetime import datetime
 from lib.design.character_parameters_editor_design import *
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QLabel, QFileDialog, QMessageBox
+from lib.classes.Character import Character
+
+# Temp folder name
+temp_folder = "temp_" + datetime.now().strftime("_%d-%m-%Y_%H-%M-%S")
+
+# resources path
+dbrb_compressor_path = os.path.join("lib", "resources", "dbrb_compressor.exe")
+
+# path images
+path_little_images = os.path.join("lib","images","little")
+path_large_images = os.path.join("lib","images","large")
+
+# base position transformations
+basePosTrans = 66723
+
+# number of bytes between each character
+sizeTrans = 33
+
+# panelPortraistlist
+miniPortraitsImage = []
+characterList = [0] * 100
+
+def del_rw(name_method, path, error):
+    os.chmod(path, stat.S_IWRITE)
+    os.remove(path)
+
+    return name_method, error
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def __init__(self, *args, **kwargs):
+
+        global miniPortraitsImage
+
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
 
@@ -20,28 +52,93 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.actionCredits.triggered.connect(self.action_credits_logic)
 
         # Load all the mini portraits
-        miniPortraits = self.panel.findChildren(QLabel)
-        path_little_images = os.path.join("lib","images","little")
+        miniPortraitsImage = self.panel.findChildren(QLabel)
+        index = 0
         for i in range(0,62):
-        	miniPortraits[i].setPixmap(QPixmap(os.path.join(path_little_images, "sc_chara_0" + miniPortraits[i].objectName().split("_")[1] + ".bmp")))
-        	miniPortraits[i].setStyleSheet("QLabel {border : 3px solid grey;}")
-        	miniPortraits[i].setDisabled(True)
+            indexChara = miniPortraitsImage[i].objectName().split("_")[1]
+            miniPortraitsImage[i].setPixmap(QPixmap(os.path.join(path_little_images, "sc_chara_0" + indexChara + ".bmp")))
+            miniPortraitsImage[i].setStyleSheet("QLabel {border : 3px solid grey;}")
+            miniPortraitsImage[i].mousePressEvent = functools.partial(self.action_change_character, index=int(indexChara))
+            miniPortraitsImage[i].setDisabled(True)
 
-        for i in range(62, len(miniPortraits)):
-        	miniPortraits[i].setStyleSheet("QLabel {border : 3px solid grey;}")
-        	miniPortraits[i].setVisible(False)
+        for i in range(62, len(miniPortraitsImage)):
+        	miniPortraitsImage[i].setStyleSheet("QLabel {border : 3px solid grey;}")
+        	miniPortraitsImage[i].setVisible(False)
 
         # Set the the main image invisible
-        self.portrait.setVisible(False)
+        self.portrait.setVisible(True)
+
+    def action_change_character(self, event, index=None):
+        self.portrait.setPixmap(QPixmap(os.path.join(path_large_images, "chara_up_chips_l_0" + str(index).zfill(2) + ".png")))
+
 
     def action_open_logic(self):
 
-		# Open spr file
-        spr_file_path_original = \
-            QFileDialog.getOpenFileName(self, "Open file", os.path.abspath(os.getcwd()), "PAK files (*.dak)")[0]
+        global temp_folder, miniPortraitsImage
+
+		# Open pak file
+        pak_file_path_original = \
+            QFileDialog.getOpenFileName(self, "Open file", os.path.abspath(os.getcwd()), "PAK files (*.pak)")[0]
+
+        # Check if the user has selected a pak format file
+        if not os.path.exists(pak_file_path_original):
+            msg = QMessageBox()
+            msg.setWindowTitle("Error")
+            msg.setText("A pak file is needed.")
+            msg.exec()
+            return
+
+        # Check if the pak file is the correct one
+        with open(pak_file_path_original, mode="rb") as pak_file:
+            pak_file.seek(78)
+            data = pak_file.read(26)
+            if data.decode('utf-8') != "operate_resident_param.pak":
+                msg = QMessageBox()
+                msg.setWindowTitle("Error")
+                msg.setText("The file you selected is not the correct one.")
+                msg.exec()
+                return
+
+        basename = os.path.basename(pak_file_path_original)
+
+        # Create a folder where we store the necessary files or delete it. If already exists,
+        # we remove every files in it
+        if os.path.exists(temp_folder):
+            rmtree(temp_folder, onerror=del_rw)
+        os.mkdir(temp_folder)
+
+        # Execute the script in a command line for the pak file
+        pak_file_path = os.path.join(os.path.abspath(os.getcwd()), temp_folder, basename.replace(".pak", "_d.pak"))
+        args = os.path.join(dbrb_compressor_path) + " \"" + pak_file_path_original + "\" \"" + pak_file_path + "\""
+        os.system('cmd /c ' + args)
+
+        # Read the file
+        with open(pak_file_path, mode="rb") as pak_file:
+
+            for i in range(0,100):
+                character = Character()
+                character.positionTrans = basePosTrans + (i * sizeTrans)
+                pak_file.seek(character.positionTrans)
+
+                # Transformation 1
+                character.transformations[0] = int.from_bytes(pak_file.read(1), byteorder='little')
+                 # Transformation 2
+                character.transformations[1] = int.from_bytes(pak_file.read(1), byteorder='little')
+                 # Transformation 3
+                character.transformations[2] = int.from_bytes(pak_file.read(1), byteorder='little')
+                 # Transformation 4
+                character.transformations[3] = int.from_bytes(pak_file.read(1), byteorder='little')
+
+                characterList[i] = character
+
+        # Enable the characters portraits
+        for i in range(0,62):
+            miniPortraitsImage[i].setEnabled(True)
 
 
     def closeEvent(self, event):
+        if os.path.exists(temp_folder):
+            rmtree(temp_folder, onerror=del_rw)
         event.accept()
 
     @staticmethod
---
+++ lib/classes/Character.py
@@ -0,0 +1,3 @@
+class Character:
+	positionTrans = 0
+	transformations = [0] * 4
