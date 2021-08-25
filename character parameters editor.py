d5581d8d9ce7779575cb591895cfaec8131d8aa1
unknown <ferrari_dani_010@hotmail.es>
2021-08-25 19:47:26 +0200

    Added a new window that the user will select the transformation for one slot

--- character parameters editor.py
+++ character parameters editor.py
@@ -3,6 +3,7 @@ import os, stat, functools
 from shutil import rmtree
 from datetime import datetime
 from lib.design.character_parameters_editor_design import *
+from lib.design.select_chara import Ui_Dialog
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QLabel, QFileDialog, QMessageBox
 from lib.classes.Character import Character
@@ -27,8 +28,15 @@ sizeTrans = 33
 
 # panelPortraistlist
 miniPortraitsImage = []
+
+# portraits object for the Select Character window
+charaSelectedCharacterWindow = 100
+miniPortraitsImageSelectCharaWindow = []
+
+# List of character with their data from the file
 characterList = []
 charaSelected = 0
+
  # Store what character has original transform version
 charactersWithTrans = [0, 5, 8, 17, 22, 24, 27, 29, 31, 34, 40, 48, 58, 63, 67, 79, 82, 85, 88]
 # Store what transformations has the character originally
@@ -45,7 +53,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def __init__(self, *args, **kwargs):
 
-        global miniPortraitsImage
+        global miniPortraitsImage, miniPortraitsImageSelectCharaWindow
 
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
@@ -59,7 +67,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.actionAuthor.triggered.connect(self.action_author_logic)
         self.actionCredits.triggered.connect(self.action_credits_logic)
 
-        # Load all the mini portraits
+        # Load all the mini portraits (main panel)
         miniPortraitsImage = self.panel.findChildren(QLabel)
         index = 0
         for i in range(0,62):
@@ -81,6 +89,15 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.transPanel.setPixmap(QPixmap(os.path.join(path_fourSlot_images, "pl_transform.png")))
         self.transPanel.setVisible(False)
 
+        # Load the Select Chara window
+        self.selectCharaWindow = QtWidgets.QMainWindow()
+        self.selectCharaUI = Ui_Dialog()
+        self.selectCharaUI.setupUi(self.selectCharaWindow)
+        miniPortraitsImageSelectCharaWindow = self.selectCharaUI.frame.findChildren(QLabel)
+        for i in range(0,100):
+            miniPortraitsImageSelectCharaWindow[i].setPixmap(QPixmap(os.path.join(path_little_images, "sc_chara_0" + str(i).zfill(2) + ".bmp")))
+
+
     def action_change_character(self, event, index=None, modifySlotTransform=False):
 
         global charaSelected
@@ -91,24 +108,49 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             # Load the portrait
             self.portrait.setPixmap(QPixmap(os.path.join(path_large_images, "chara_up_chips_l_0" + str(index).zfill(2) + ".png")))
 
-            # Load the transformations
+            # Load the transformations for the panel transformations
             transformations = characterList[index].transformations
-            self.transSlotPanel0.setPixmap(QPixmap(os.path.join(path_little2_images, "sc_chara_s_0" + str(transformations[0]).zfill(2) + ".png")))
-            self.transSlotPanel1.setPixmap(QPixmap(os.path.join(path_little2_images, "sc_chara_s_0" + str(transformations[1]).zfill(2) + ".png")))
-            self.transSlotPanel2.setPixmap(QPixmap(os.path.join(path_little2_images, "sc_chara_s_0" + str(transformations[2]).zfill(2) + ".png")))
-            self.transSlotPanel3.setPixmap(QPixmap(os.path.join(path_little2_images, "sc_chara_s_0" + str(transformations[3]).zfill(2) + ".png")))
+            # Change panel transformations and their interactions
+            if transformations[0] != 100:
+                self.transSlotPanel0.setPixmap(QPixmap(os.path.join(path_little2_images, "sc_chara_s_0" + str(transformations[0]).zfill(2) + ".png")))
+                self.transSlotPanel0.mousePressEvent = functools.partial(self.open_select_chara_window, index=transformations[0])
+                self.transSlotPanel0.setVisible(True)
+            else:
+                self.transSlotPanel0.setPixmap(QPixmap())
+                self.transSlotPanel0.mousePressEvent = functools.partial(self.open_select_chara_window, index=100)
+            if transformations[1] != 100:
+                self.transSlotPanel1.setPixmap(QPixmap(os.path.join(path_little2_images, "sc_chara_s_0" + str(transformations[1]).zfill(2) + ".png")))
+                self.transSlotPanel1.mousePressEvent = functools.partial(self.open_select_chara_window, index=transformations[1])
+                self.transSlotPanel1.setVisible(True)
+            else:
+                self.transSlotPanel1.setPixmap(QPixmap())
+                self.transSlotPanel1.mousePressEvent = functools.partial(self.open_select_chara_window, index=100)
+            if transformations[2] != 100:
+                self.transSlotPanel2.setPixmap(QPixmap(os.path.join(path_little2_images, "sc_chara_s_0" + str(transformations[2]).zfill(2) + ".png")))
+                self.transSlotPanel2.mousePressEvent = functools.partial(self.open_select_chara_window, index=transformations[2])
+                self.transSlotPanel2.setVisible(True)
+            else:
+                self.transSlotPanel2.setPixmap(QPixmap())
+                self.transSlotPanel2.mousePressEvent = functools.partial(self.open_select_chara_window, index=100)
+            if transformations[3] != 100:
+                self.transSlotPanel3.setPixmap(QPixmap(os.path.join(path_little2_images, "sc_chara_s_0" + str(transformations[3]).zfill(2) + ".png")))
+                self.transSlotPanel3.mousePressEvent = functools.partial(self.open_select_chara_window, index=transformations[3])
+                self.transSlotPanel3.setVisible(True)
+            else:
+                self.transSlotPanel3.setPixmap(QPixmap())
+                self.transSlotPanel3.mousePressEvent = functools.partial(self.open_select_chara_window, index=100)                                           
 
             # Modify the slots of the transformations in the main panel
             if modifySlotTransform:
 
-                # Disable all the transformations slot if it has been activated
+                # Disable all the transformations of the slots if it has been activated in the main panel
                 if self.label_trans_0.isVisible():
                     self.label_trans_0.setVisible(False)
                     self.label_trans_1.setVisible(False)
                     self.label_trans_2.setVisible(False)
                     self.label_trans_3.setVisible(False)
-                    self.label_trans_4.setVisible(False)
 
+                # Get the original transformations for the character
                 if index in charactersWithTrans:
                     transformations = charactersWithTransIndex[charactersWithTrans.index(index)]
                     numTransformations = len(transformations)
@@ -127,16 +169,11 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                                 if numTransformations > 3:
                                     self.label_trans_3.setPixmap(QPixmap(os.path.join(path_little_images, "sc_chara_0" + str(transformations[3]).zfill(2) + ".bmp")))
                                     self.label_trans_3.mousePressEvent = functools.partial(self.action_change_character, index=transformations[3], modifySlotTransform=False)
-                                    self.label_trans_3.setVisible(True)
-                                if numTransformations > 4:
-                                    self.label_trans_4.setPixmap(QPixmap(os.path.join(path_little_images, "sc_chara_0" + str(transformations[4]).zfill(2) + ".bmp")))
-                                    self.label_trans_4.mousePressEvent = functools.partial(self.action_change_character, index=transformations[4], modifySlotTransform=False)
-                                    self.label_trans_4.setVisible(True)
+                                    self.label_trans_3.setVisible(True)                                                                
 
 
             # Store the actual index selected of the char
             charaSelected = index
-
 
 
     def action_open_logic(self):
@@ -219,11 +256,31 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         # Show the transform panel
         self.transSlotPanel0.setPixmap(QPixmap(os.path.join(path_little2_images, "sc_chara_s_0" + str(transformations[0]).zfill(2) + ".png")))
+        self.transSlotPanel0.mousePressEvent = functools.partial(self.open_select_chara_window, index=transformations[0])
         self.transSlotPanel1.setPixmap(QPixmap(os.path.join(path_little2_images, "sc_chara_s_0" + str(transformations[1]).zfill(2) + ".png")))
+        self.transSlotPanel1.mousePressEvent = functools.partial(self.open_select_chara_window, index=transformations[1])
         self.transSlotPanel2.setPixmap(QPixmap(os.path.join(path_little2_images, "sc_chara_s_0" + str(transformations[2]).zfill(2) + ".png")))
+        self.transSlotPanel2.mousePressEvent = functools.partial(self.open_select_chara_window, index=transformations[2])
+        self.transSlotPanel3.setPixmap(QPixmap(os.path.join(path_little2_images, "sc_chara_s_0" + str(transformations[3]).zfill(2) + ".png")))
+        self.transSlotPanel3.mousePressEvent = functools.partial(self.open_select_chara_window, index=transformations[3])
         self.transPanel.setVisible(True)
 
 
+    def open_select_chara_window(self, event, index):
+
+        global charaSelectedCharacterWindow
+        # Avoid adding the red border to the character transform selected
+        if charaSelectedCharacterWindow != index:
+            # If the index is 100 (means there's no character transformation), we will remove the red border for the previous character transform panel
+            if index == 100:
+                miniPortraitsImageSelectCharaWindow[charaSelectedCharacterWindow].setStyleSheet("QLabel {}")
+            else:
+                miniPortraitsImageSelectCharaWindow[index].setStyleSheet("QLabel {border : 3px solid red;}")
+                if charaSelectedCharacterWindow != 100:
+                    miniPortraitsImageSelectCharaWindow[charaSelectedCharacterWindow].setStyleSheet("QLabel {}")
+                
+            charaSelectedCharacterWindow = index
+        self.selectCharaWindow.show()
 
     def closeEvent(self, event):
         if os.path.exists(temp_folder):
